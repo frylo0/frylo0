4d33872efc5b5b35fb9238e5bfc59a34018c343b
frylo0 <frylo.d.ts@gmail.com>
2024-03-25 23:18:56 +0300

    v2 of readme

--- templates/repo.php
+++ templates/repo.php
@@ -1,9 +1,10 @@
-<?php function repo($username, $repo_name, $width = 278) { ?><?php global $theme; ?>
+<?php function repo($username, $repo_name, $width = 278) { ?>
 <?php start_minimize() ?>
+    <?php global $theme; ?>
 
     <?php 
         $img = "https://github-readme-stats.vercel.app/api/pin/" . 
-            "?username=$username&repo=$repo_name&theme=react" . 
+            "?username=$username&repo=$repo_name" . 
             //"&bg_color=1F222E&title_color=F85D7F&icon_color=F8D866" . 
             "&theme=$theme" . 
             "&hide_border=true&show_icons=false"; 
